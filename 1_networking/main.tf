e671b26c2bfdccb38d76aa43dc96d896ce52d57a
jblessingHC <85644461+jblessingHC@users.noreply.github.com>
2024-06-19 10:11:20 -0400

    Update main.tf
    Latest Providers Releases.  July 2024

--- 1_networking/main.tf
+++ 1_networking/main.tf
@@ -7,12 +7,12 @@ terraform {
 
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.8.0"
+      version = "~> 5.54.1"
     }
 
     hcp = {
       source  = "hashicorp/hcp"
-      version = "~> 0.66.0"
+      version = "~> 0.92.0"
     }
   }
 }
